e8aa94d05d391ec0792797f722e2a50e53ee2169
GitHub Action <action@github.com>
2020-11-09 06:09:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/labnof/ctime/MyBundle.kt
+++ src/main/kotlin/com/github/labnof/ctime/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.labnof.ctime
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/labnof/ctime/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/labnof/ctime/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.labnof.ctime.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.labnof.ctime.MyBundle
 
 class MyApplicationService {
 
